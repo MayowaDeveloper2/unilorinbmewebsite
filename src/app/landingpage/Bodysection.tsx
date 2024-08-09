2b494c9f9ece07f79a612f842b73606bc6cf8816
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-08-09 20:19:40 +0100

    all task

--- src/app/landingpage/Bodysection.tsx
+++ src/app/landingpage/Bodysection.tsx
@@ -1,5 +1,7 @@
 
 
+/* eslint-disable react/no-unescaped-entities */ 
+
 import Image from "next/image";
 import cardimage1 from "../images/image-beautiful-cheerful-girl-with-toothy-smile-writes-notes-notebook-with-pencil.jpg";
 import cardimage2 from "../images/friends-learning-study-group.jpg";
@@ -21,7 +23,7 @@ import Newspost from "./news";
 const cardimages = [
     { src: cardimage1, alt: "cardimage", content: "Prospective Students" },
     { src: cardimage2, alt: "cardimage", content: "Current Students" },
-    { src: cardimage3, alt: "cardimage", content: "Faculty  Research" },
+    { src: cardimage3, alt: "cardimage", content: "Faculty & Research" },
     { src: cardimage4, alt: "cardimage", content: "Alumni Stories" },
     { src: cardimage5, alt: "cardimage", content: "Student Stories" },
 ];
@@ -29,6 +31,7 @@ const cardimages = [
 export default function Bodysection() {
     return (
         <>
+            
             <div>
                 <div className="flex lg:items-center lg:justify-center lg:mt-10 flex-row overflow-x-auto space-x-4 p-4">
                     {cardimages.map((cardimage, index) => (
@@ -73,12 +76,12 @@ export default function Bodysection() {
                             <Image src={bmeevent} alt="" className="lg:w-[400px] mb-4 rounded-3xl lg:mb-0" />
                             <div className="flex flex-col lg:items-center lg:justify-center lg:ml-4">
                                 <div className="text-lg text-black font-semibold mb-4 lg:mb-0 lg:mr-4">
-                                    1ST Department Lecture, Symposium, and Biomedical Engineering Exhibition
+                                    1ST Department Lecture, Symposium, and Biomedical Engineering Exhibition (BMEX)
                                 </div>
                                 <hr className="h-px my-8 border-0 dark:bg-gray-700"></hr>
                                 <div className="w-full pl-4 flex">
                                     <span className="flex items-center mr-7"> 
-                                        <FaRegCalendarDays className="mr-2"/> 2  4 August, 2024
+                                        <FaRegCalendarDays className="mr-2"/> 2 - 4 August, 2024
                                     </span>
                                     <span className="flex items-center">
                                         <GoClock className="mr-2"/> 10:00am
@@ -95,8 +98,8 @@ export default function Bodysection() {
                         <Image className="lg:w-[800px]" src={dooter} alt="dooter" />
                         <div className="flex-col px-3">
                             <div className="text-white lg:text-2xl font-bold mt-3">Student Spotlight</div>
-                            <FaQuoteLeft className="text-customeYellow text-5xl mt-7"/> 
-                            <p className="w-[300px] lg:text-4xl text-white"> hi</p>
+                            <FaQuoteLeft className="text-customeYellow text-5xl mt-7"/>
+                            <span className="w-[300px] lg:text-4xl text-white"> Pursing a degree in Biomedical Engineering at Univerisity of Ilorin has taught me to be resilence as the course seem highly technical. I want to make lasting solution to problens in the healthcare sector with my skills acquired in Bioinformatics, and others</span>
                             <div className="mt-7 text-white lg:mt-96">
                                 <div className="font-bold">Dooter Aondoakaa</div>
                                 <div>Biomedical Engineering, class of 2023</div>
@@ -105,25 +108,25 @@ export default function Bodysection() {
 
                         </div>
                     </div>
-                </div> 
+                </div>
                 <div className="mt-5 px-5 lg:px-20">
                     <div>
-                        <div className="text-2xl text-black font-bold mb-4 ml-4"> NEWS </div>
+                        <div className="text-2xl text-black font-bold mb-4 ml-4">NEWS</div>
                         <Newspost/>
                     </div>
                 </div>
-                {/* <div className="mt-6 mb-6 w-full h-[400px] px-5 md:px-3 lg:px-20  flex">
+                <div className="mt-6 mb-6 w-full h-[400px] px-5 md:px-3 lg:px-20  flex">
                     <div className="flex h-full items-center flex-col lg:flex-row">
                         <iframe
                             className="w-[300px] lg:w-[700px] md:w-[400px] rounded-3xl h-64 md:h-80 lg:h-96"
                             allow="autoplay; fullscreen"
                             allowFullScreen
-                            src="https://www.youtube.com/embed/IJ_14huTj6E?autoplay=1&amp;mute=1"
+                            src="https://www.youtube.com/embed/IJ_14huTj6E?autoplay=1&mute=1"
                         />
                         <div className="w-[350px] lg:w-[600px] md:w-[400px] ml-5 text-2xl font-bold mt-4 lg:mt-0">Welcome Message From The Head of Department of Biomedical Engineering</div>
 
                     </div>
-                </div> */}
+                </div>
                 
             </div>
         </>
