3c71c2e9615504660476423617ecab276b4eebb4
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2024-08-09 19:38:47 +0100

    all task

--- src/app/landingpage/Bodysection.tsx
+++ src/app/landingpage/Bodysection.tsx
@@ -12,8 +12,8 @@ import ourinnovations from "../images/a-travel-mobility-scooter-with-a-shopping-
 import bmeevent from "../images/bme event.jpg";
 import { FaRegCalendarDays } from "react-icons/fa6";
 import { GoClock } from "react-icons/go";
-// import { FaQuoteLeft } from "react-icons/fa";
-// import dooter from "../images/dooter.jpeg";
+import { FaQuoteLeft } from "react-icons/fa";
+import dooter from "../images/dooter.jpeg";
 import Newspost from "./news";
 
 
@@ -90,13 +90,13 @@ export default function Bodysection() {
                         </div>
                     </div>
                 </div>
-                {/* <div className="flex flex-col lg:flex-row bg-customBlue">
+                <div className="flex flex-col lg:flex-row bg-customBlue">
                     <div className="flex flex-col lg:flex-row">
                         <Image className="lg:w-[800px]" src={dooter} alt="dooter" />
                         <div className="flex-col px-3">
                             <div className="text-white lg:text-2xl font-bold mt-3">Student Spotlight</div>
                             <FaQuoteLeft className="text-customeYellow text-5xl mt-7"/> 
-                            <span className="w-[300px] lg:text-4xl text-white"> Pursing a degree in Biomedical Engineering at Univerisity of Ilorin has taught me to be resilence as the course seem highly technical. I want to make lasting solution to problens in the healthcare sector with my skills acquired in Bioinformatics, and others</span>
+                            <p className="w-[300px] lg:text-4xl text-white"> hi</p>
                             <div className="mt-7 text-white lg:mt-96">
                                 <div className="font-bold">Dooter Aondoakaa</div>
                                 <div>Biomedical Engineering, class of 2023</div>
@@ -105,7 +105,7 @@ export default function Bodysection() {
 
                         </div>
                     </div>
-                </div> */}
+                </div> 
                 <div className="mt-5 px-5 lg:px-20">
                     <div>
                         <div className="text-2xl text-black font-bold mb-4 ml-4"> NEWS </div>
